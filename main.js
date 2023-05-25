778797a92538ba42d633c87e4cfd77e71e659871
cloudhuh <claudiu.huhulia@gmail.com>
2023-05-25 16:02:48 +0200

    done

--- main.js
+++ main.js
@@ -1,8 +1,31 @@
 console.log('JS OK')
 
 /* 
-1.
-
-
-
+1. Recupero l'elemento dal DOM
+2. Chiedi all'utente il nome
+3. Chiedi il suo Cognome
+4. Chiedi il colore preferito
+5. Salviamo i dati nelle rispettive variabili
+6. Sommiamo Nome cognome e colore preferito
+7. Aggiugniamo 21
+8. Inseriamo il risultato nell'elemento del DOM
 */
+
+const passw= document.getElementById('OutputPass');
+console.log(passw);
+
+const num='21';
+
+const nome=prompt('Qual è il tuo nome?', 'Claudio');
+console.log(nome);
+
+const cognome=prompt('Qual è il tuo cognome?', 'Huhulia');
+console.log(cognome);
+
+const colore=prompt('Qual è il tuo colore preferito?', 'Verde');
+console.log(colore);
+
+const pass= nome + cognome + colore + num;
+console.log(pass);
+
+passw.innerText=pass;
